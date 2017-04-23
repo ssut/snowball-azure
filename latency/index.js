aaba50138f59d2676faf346bc3ecc278a5966f12
SuHun Han <ssut@ssut.me>
2017-04-23 09:18:12 +0900

    httpstat

--- latency/index.js
+++ latency/index.js
@@ -1,50 +1,57 @@
-const axios = require('axios');
+"use strict";
+const httpstat = require('httpstat');
 
-const newResponse = (error, statusCode, latency) => ({
+const newResponse = (error, statusCode, bodySize, latency) => ({
   error: error,
   status: statusCode,
+  body: bodySize,
   latency: latency,
 });
-
-const getElapsed = start => {
-  const hrTime = process.hrtime(start);
-  return Math.floor(hrTime[0] * 1000 + hrTime[1] / 1000000);
-};
 
 module.exports = (context, req) => {
   if (req.query.url === undefined || req.query.method === undefined) {
     context.res = {status: 400};
     return context.done();
   }
+  const url = req.query.url;
+  const body = req.query.body;
   const q = {
-    url: req.query.url,
     method: req.query.method,
     timeout: req.query.timeout ? parseInt(req.query.timeout) : 10000,
-    body: req.query.body,
     headers: req.query.headers ? JSON.parse(req.query.headers) : {},
   };
 
-  const started = process.hrtime();
-  axios(q)
-  .then(resp => {
-    const elapsed = getElapsed(started);
-    const statusCode = resp.status;
+  let timeout = setTimeout(() => {
+    timeout = null;
+    context.res = {status: 200, body: newResponse("TIMEOUT", -1, -1 {})};
+    contxt.done();
+  }, 1 + q.timeout);
 
-    const sbResp = newResponse("", statusCode, elapsed);
-    context.res = {status: 200, body: sbResp, headers: {'Content-Type': 'application/json'}};
-    context.done();
-  })
-  .catch(err => {
-    const elapsed = getElapsed(started);
-    let error = "";
-    if (err.code === 'ECONNABORTED') {
-      error = "TIMEOUT";
-    } else {
-      error = err.reason;
+  httpstat(url, q, body).then(result => {
+    if (timeout === null) return;
+    clearTimeout(timeout);
+    const statusCode = result.response.statusCode;
+    const bodySize = result.response.body.length;
+    const proto = result.url.protocol;
+    const time = result.time;
+    const latency = {
+      dns: time.onLookup - time.begin,
+      tcp: time.onConnect - time.onLookup,
+      tls: time.onSecureConnect - time.onConnect,
+      server: time.onTransfer - (proto === 'https:' ? time.onSecureConnect : time.onConnect),
+      transfer: time.onTotal - time.onTransfer,
+    };
+    if (proto === 'http:') {
+      delete latency.tls;
     }
 
-    const errResp = newResponse(error, -1, elapsed);
-    context.res = {status: 200, body: errResp, headers: {'Content-Type': 'application/json'}};
+    context.res = {status: 200, body: newResponse("", statusCode, bodySize, latency)};
     context.done();
-  });
+  }).catch(e => {
+    if (timeout === null) return;
+    clearTimeout(timeout);
+    console.error(e);
+    context.res = {status: 200, body: newResponse(e.toString(), -1, -1 {]})};
+    context.done();
+  })
 };
